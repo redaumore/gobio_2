1b9141dc6c737dc6bd77ff923d82f17be3a68129
Rolando Daumas <redaumore@gmail.com>
2024-10-18 20:37:47 -0300

    Add CRM components

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import GobioHeader from './components/GobioHeader';
 import GobioFooter from './components/GobioFooter';
@@ -11,6 +11,20 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const location = useLocation();
   const bgPages = ['/contacto', '/muestra', '/faqs'];
   const shouldApplyBg = bgPages.includes(location.pathname);
+
+  useEffect(() => {
+    const scriptPaths = ['/contacto', '/muestra'];
+    if (scriptPaths.includes(location.pathname)) {
+      const script = document.createElement('script');
+      script.src = 'https://leads.godixital.com/js/leads_form_trigger.js';
+      script.async = true;
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, [location.pathname]);
 
   return (
     <div className="flex flex-col min-h-screen font-montserrat">
